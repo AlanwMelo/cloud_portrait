4fb371596ab8fe169fd881a5db3827630cc64e16
Alan Melo <amelo967@gmail.com>
2022-10-24 17:23:09 -0300

    layout: primeiros passos

---
+++ android/app/src/main/kotlin/com/example/cloud_portrait/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cloud_portrait
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:cloud_portrait/presentation/listViewer.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cloud Portrait',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Alan\'s Collections'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(child: ListViewer()),
+            bottomBar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bottomBar() {
+    return Container(
+      height: 60,
+      color: Colors.blue,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: Container(
+              child: newButton(),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: goToPresentation(),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: profile(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  newButton() {
+    return Container(
+      child: bottomBarIcons(Icons.add),
+    );
+  }
+
+  goToPresentation() {
+    return Container(
+      child: bottomBarIcons(Icons.playlist_play_rounded),
+    );
+  }
+
+  profile() {
+    return Container(
+      child: bottomBarIcons(Icons.account_box_rounded),
+    );
+  }
+
+  bottomBarIcons(IconData icon) {
+    return Icon(
+      icon,
+      size: 35,
+      color: Colors.white.withOpacity(0.9),
+    );
+  }
+}
