b30683df4ff6910fd8b00c8ef9b4750d8a03b9d8
Alan Melo <amelo967@gmail.com>
2022-10-26 14:42:15 -0300

    Carrossel preparado (sem dados)

--- lib/data/carousel_slider.dart
+++ lib/data/carousel_slider.dart
@@ -1,5 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wakelock/wakelock.dart';
 
 class PortraitCarousel extends StatefulWidget {
   const PortraitCarousel({super.key});
@@ -9,11 +12,46 @@ class PortraitCarousel extends StatefulWidget {
 }
 
 class _PortraitCarouselState extends State<PortraitCarousel> {
+
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Wakelock.enable();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    Wakelock.disable();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: CarouselSlider(
+          options: CarouselOptions(height: 400.0, enableInfiniteScroll: false, viewportFraction: 1),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(color: Colors.amber),
+                    child: Text(
+                      'text $i',
+                      style: TextStyle(fontSize: 16.0),
+                    ));
+              },
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
