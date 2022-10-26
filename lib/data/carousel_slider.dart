7c351be3d625be7bc903bd2779acb17d5623ae84
Alan Melo <amelo967@gmail.com>
2022-10-26 15:25:29 -0300

    visibility notifier do carrossel criado

--- lib/data/carousel_slider.dart
+++ lib/data/carousel_slider.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 import 'package:wakelock/wakelock.dart';
 
 class PortraitCarousel extends StatefulWidget {
@@ -12,7 +13,6 @@ class PortraitCarousel extends StatefulWidget {
 }
 
 class _PortraitCarouselState extends State<PortraitCarousel> {
-
   @override
   void initState() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
@@ -31,22 +31,60 @@ class _PortraitCarouselState extends State<PortraitCarousel> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
+      body: SizedBox(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: CarouselSlider(
-          options: CarouselOptions(height: 400.0, enableInfiniteScroll: false, viewportFraction: 1),
+          options: CarouselOptions(
+              height: 400.0, enableInfiniteScroll: false, viewportFraction: 1),
           items: [1, 2, 3, 4, 5].map((i) {
             return Builder(
               builder: (BuildContext context) {
-                return Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(color: Colors.amber),
-                    child: Text(
-                      'text $i',
-                      style: TextStyle(fontSize: 16.0),
-                    ));
+                ValueNotifier<bool> isVisible = ValueNotifier(false);
+
+                visible() async {
+                  bool result = false;
+
+                  isVisible.addListener(() {
+                    result = isVisible.value;
+                  });
+
+                  while (!result) {
+                    await Future.delayed(const Duration(seconds: 1));
+                  }
+                  return result;
+                }
+
+                return VisibilityDetector(
+                  key: UniqueKey(),
+                  child: FutureBuilder(
+                    future: visible(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<dynamic> snapshot) {
+                      if (snapshot.hasData) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(color: Colors.amber),
+                            child: Text(
+                              'text $i',
+                              style: TextStyle(fontSize: 16.0),
+                            ));
+                      } else {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                    },
+                  ),
+                  onVisibilityChanged: (visibilityInfo) {
+                    var visiblePercentage =
+                        visibilityInfo.visibleFraction * 100;
+                    if (visiblePercentage == 100) {
+                      isVisible.value = true;
+                      debugPrint(
+                          'Widget ${visibilityInfo.key} is ${visiblePercentage}% visible');
+                    }
+                  },
+                );
               },
             );
           }).toList(),
